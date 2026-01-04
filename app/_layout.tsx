2d57fa200493e2e13ab436d9e1822bc6325f4d28
Ezra <ezrachin05@gmail.com>
2026-01-04 17:55:59 +0800

    Feature: Added Location Context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,40 @@
-import { queryClient } from '@/app/providers/QueryClient';
-import { SplashScreenController } from '@/components/splash';
-import { SessionProvider, useSession } from '@/contexts/AuthContext';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from "@/app/providers/QueryClient";
+import { SplashScreenController } from "@/components/splash";
+import { SessionProvider, useSession } from "@/contexts/AuthContext";
+import { LocationProvider } from "@/contexts/LocationContext";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 // export const unstable_settings = {
 //   anchor: '(tabs)',
 // };
 
-function RootNavigator(){
+function RootNavigator() {
   const { session } = useSession();
   return (
     <Stack>
-        <Stack.Protected guard={!!session}>
-            {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Protected guard={!!session}>
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
-            <Stack.Screen name="(app)" options= {{ headerShown: false }} />
-        </Stack.Protected>
-        <Stack.Protected guard={!session}>
-          <Stack.Screen name ="(auth)" options={{ headerShown:false }} />
-        </Stack.Protected>
+        <Stack.Screen name="(app)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack.Protected>
     </Stack>
-  )
-
+  );
 }
 
-export default function Root(){
-  return(
+export default function Root() {
+  return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider>
-        <SplashScreenController/>
-        <RootNavigator/>
+        <LocationProvider>
+          <SplashScreenController />
+          <RootNavigator />
+        </LocationProvider>
       </SessionProvider>
-      </QueryClientProvider>
-  )
+    </QueryClientProvider>
+  );
 }
---
+++ contexts/LocationContext.tsx
@@ -0,0 +1,66 @@
+import * as Location from 'expo-location'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+type LocationContextType = {
+    location: Location.LocationObject | null
+    address: Location.LocationGeocodedAddress | null
+    errorMsg: string | null
+    loading: boolean 
+}
+
+const LocationContext = createContext<LocationContextType>({
+    location:null,
+    address:null,
+    errorMsg:null,
+    loading:true,
+})
+
+export const LocationProvider = ({ children }: { children: React.ReactNode}) => {
+    const [location, setLocation ] = useState<Location.LocationObject | null>(null)
+    const [address, setAddress] = useState<Location.LocationGeocodedAddress | null> (null)
+    const [errorMsg, setErrorMsg ] = useState<string|null>(null)
+    const [loading, setLoading] =useState(true)
+
+    useEffect(() => {
+        let isMounted = true;
+        ;(async() => {
+            try{
+                const { status } = await Location.requestBackgroundPermissionsAsync()
+                if (status !== 'granted'){
+                    if (isMounted) setErrorMsg('Location permission denied ')
+                        return
+                }
+                const currentLocation = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Balanced})
+
+                if (!isMounted) return
+                setLocation(currentLocation)
+
+                const [place ] = await Location.reverseGeocodeAsync({
+                    latitude: currentLocation.coords.latitude,
+                    longitude: currentLocation.coords.longitude
+                })
+                if (!isMounted) return 
+                setAddress(place)
+
+
+            }catch(err){
+                if (isMounted) setErrorMsg('Failed to get Location')
+            }finally{
+                if (isMounted) setLoading(false)
+            }
+
+
+        })()
+        return () => {
+            isMounted = false 
+        }
+    },[])
+
+    return (
+        <LocationContext.Provider value={{ location, address, errorMsg, loading}}>
+            {children}
+        </LocationContext.Provider>
+    )
+}
+
+export const useLocationContext = () => useContext(LocationContext)
